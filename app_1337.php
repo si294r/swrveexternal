21ff3262ffad9452fe89d744e1debf912c0e5290
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-05-04 17:12:20 +0700

    Initial Commit

---
+++ app_1337.php
@@ -0,0 +1,59 @@
+<?php
+
+(php_sapi_name() === "cli") OR exit("Script access is only allowed from command line");
+
+require '/var/www/vendor/autoload.php';
+
+/*
+ * Function Definition
+ */
+
+function get_list_filename() {
+    exec("find /root/swrveexternal-alegrium/ -path *app-1377*.log.gz", $output); // find all log.gz
+    asort($output);
+    $output = array_values($output);
+    return $output;
+}
+
+/*
+ * Configuration, moved to swrve.php
+ */
+
+//$db_host = 'mongodb_server';
+//$db_user = 'user';
+//$db_pass = 'password';
+$db_name = 'swrve';
+
+include "/var/www/swrve.php";
+
+/*
+ * Main Script
+ */
+
+$connection_string = "mongodb://"
+        . $db_user . ":"
+        . $db_pass . "@"
+        . $db_host . "/"
+        . $db_name;
+
+$client = new MongoDB\Client($connection_string); // create object client 
+$db = $client->$db_name; // select database
+
+$list_filename = get_list_filename();
+foreach ($list_filename as $filename) {
+    // 1. Cek status namafile di database
+    $status = false;
+    $document = $db->swrvelog->findOne(['filename' => $filename, 'app' => '1337']);
+    if (is_object($document)) {
+        $status = true;
+    }
+
+    // 2. Import ke mongodb jika belum pernah di import
+    if ($status == false) {
+        exec("gunzip -k -f --verbose $filename");
+        $filelog = str_replace("log.gz", "log", $filename);
+        exec("mongoimport --host $db_host --db $db_name --collection swrveexternal_1377 --file $filelog --username $db_user --password $db_pass");
+        exec("rm $filelog");
+        $db->swrvelog->insertOne(array('filename' => $filename, 'app' => '1337'));
+    }
+}
